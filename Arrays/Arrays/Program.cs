cfe0bd29cb637ca0d58ee2254bfeb57318b163b8
Kavyapuli <pulikavya10@gmail.com>
2025-09-15 20:01:01 +0530

    chore: initial repo structure with arrays, strings, linked list, stack-queue

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class Program
+    {
+        //  Write a program to take 5 numbers as input and print them.
+
+        public void PrintNums()
+        {
+            int[] arr = new int[5];
+
+            Console.WriteLine("Enter 5 numbers:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("You entered:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        //find the maxm number in array
+
+        public void MaxNum()
+        {
+            int[] arr = { 3, 5, 2, 9, 6, 10, 8 };
+            int max = arr[0];
+            for(int i=0;i<arr.Length; i++)
+            {
+                if (arr[i]>max)
+                {
+                    max=arr[i];
+                }
+            }
+            Console.WriteLine($"max num in array of hardcoded: {max}");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.PrintNums();
+            p.MaxNum();
+
+            // 🔹 Keeps console open until user presses Enter
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Data Structures in C#
+This repo contains my Data Structures and Algorithms practice in C#, organized topic by topic.
